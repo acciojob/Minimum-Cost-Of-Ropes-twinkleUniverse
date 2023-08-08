8dc8e371c2c39c224753f8a824b4c0428cfe9b84
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-08-08 18:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	const div=document.getElementById("result");
+  const input=document.getElementById("rope-lengths")
+	let text=input.value;
+	let array = text.split(",").map(function (num) {
+		return parseInt(num);
+	});
+	let minCost=0;
+	while(array.length>1){
+	 array.sort((a, b) => a - b);
+		//console.log(array);
+		let sum=array.shift()+array.shift();
+		minCost+=sum
+		array.push(sum);
+	}
+	console.log(minCost);
+	div.innerText="result: "+minCost;
+	//p.innerText="result: "+minCost;
+	//div.appendChild(p);
 }  
